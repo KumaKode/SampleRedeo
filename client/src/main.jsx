bf6c4aa7bfd7656b4dd51902c3fc5900031e6bf6
mrxain699 <zaininam699@gmail.com>
2024-03-26 16:54:27 +0500

    Update Signin Page UI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { JobContextProvider } from './Context/JobContext';
-import './index.css';
-import 'swiper/swiper.css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/autoplay';
-import 'react-toastify/dist/ReactToastify.css';
-import 'rc-slider/assets/index.css';
-import { ToastContainer } from 'react-toastify';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { JobContextProvider } from "./Context/JobContext";
+import "./index.css";
+import "swiper/swiper.css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+import "react-toastify/dist/ReactToastify.css";
+import "rc-slider/assets/index.css";
+import { ToastContainer } from "react-toastify";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <JobContextProvider>
       <ToastContainer
