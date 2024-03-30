e4b13622bc7ee98c47bd28871e12b84d43d26bc7
Abdul Quim <abdul.quim99@gmail.com>
2024-03-30 16:24:41 +0500

    Fix: cors fix

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const { ServerConfig, Logger } = require("./config");
 const apiRoutes = require("./routes");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use(passport.initialize());
 
 app.use("/api", apiRoutes);
