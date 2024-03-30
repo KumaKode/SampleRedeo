5ea2259d89415b83140650025506d21596d3f5b2
Abdul Quim <abdul.quim99@gmail.com>
2024-03-30 16:12:35 +0500

    Update: Added CORS in app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const passport = require("passport");
+const cors = require("cors");
 
 const { ServerConfig, Logger } = require("./config");
 const apiRoutes = require("./routes");
@@ -7,6 +8,7 @@ const apiRoutes = require("./routes");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(passport.initialize());
 
 app.use("/api", apiRoutes);
