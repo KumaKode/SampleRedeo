80e7c74a010d112911714e54ea01663b3993b79d
Abdul Quim <abdul.quim99@gmail.com>
2024-03-30 21:27:20 +0500

    UPDATE: added .sequelizerc file for railway

---
+++ src/config/config.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+// prettier-ignore
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+},
+"test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+},
+"production": {
+    "username": process.env.MYSQLUSER || "root",
+    "password": process.env.MYSQLPASSWORD || null,
+    "database": process.env.MYSQLDATABASE || "database_production",
+    "host": process.env.MYSQLHOST || "127.0.0.1",
+    "dialect": "mysql"
+}
+};
