504bfd9f2eb574e0e72aa7155981dd097cb5f7a0
Abdul Quim <abdul.quim99@gmail.com>
2024-04-06 20:38:53 +0500

    FIX: changed local db env var

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 module.exports = {
   "development": {
     "username": "root",
-    "password": process.env.LOCAL_DB_PASSWORD || ServerConfig.LOCAL_DB_PASSWORD,
+    "password": ServerConfig.LOCAL_DB_PASSWORD,
     "database": "Redeo",
     "host": "127.0.0.1",
     "dialect": "mysql"
